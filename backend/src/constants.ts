3c058804e720615f31be967a97fa33dc5f4a01ba
dan-atack <dan_atack@hotmail.com>
2023-03-25 22:48:16 -0400

    Updated backend environment variable to use db alias instead of IP number

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,7 +1,7 @@
 // Values from ENVIRONMENT VARIABLES and other constants are loaded here:
 
 // get .env values or supply defaults
-const dbAddress: string = process.env.DB_IP_ADDRESS as string || 'localhost';
+const dbAddress: string = process.env.DB_CONTAINER_NAME as string || 'localhost';
 
 // export values
 export const constants = {
