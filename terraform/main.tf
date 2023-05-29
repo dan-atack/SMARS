f15a17454049d6c9598b29005de5bb1ce1e62aca
dan-atack <dan_atack@hotmail.com>
2023-05-28 22:17:41 -0400

    Changed key pair to previously created key

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,10 +16,5 @@ provider "aws" {
 resource "aws_instance" "smars_prod_server" {
   ami           = "ami-0a695f0d95cefc163"
   instance_type = "t2.small"
-  key_name      = "smars_prod_key_pair"
-}
-
-resource "aws_key_pair" "smars_prod_key_pair" {
-  key_name   = "smars_prod_key_pair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0AaFU3jSUgENrKIf6Xcqq7xPGup582qrKn+919LsUYZFb6cID1igSv3CvXZdnr8F18IFJ61oVv9pHf48/6kTItBvPXm4d6Pbl8Amy14bvUhyfbBcpukmcQ6HPP48LnvbHeVMNBKMNvFHeFs2/EsroXy8/vQq4uDwG+qISVOB6OIPvS/A+Q5KttISzzDbnGQag21RoCjCY1B25D+GH6/fqAyWmMYokelbThmaKOK9CsMI6MEfS0zy4M/u68rHVAvBfSyF709W5SetNH8Lh+4ZuWZzbUtxjssmhAYNdIViTX5s3r8GTJGPcYcod/7hjPQQJQ9e2Wg9M89Nle6AbPhY9"
+  key_name      = "SMARS_Prod_EC2"
 }
