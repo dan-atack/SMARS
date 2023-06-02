fb387fa86550a5aa663af1df24cbddd27d4ddb9a
dan-atack <dan_atack@hotmail.com>
2023-06-02 19:03:27 -0400

    Reformatted TF file

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,34 +14,34 @@ provider "aws" {
 }
 
 resource "aws_security_group" "smars_prod_sg" {
-    description = "SMARS Production Server Security Group"
-    name = "SMARS_Prod_SG"
+  description = "SMARS Production Server Security Group"
+  name = "SMARS_Prod_SG"
 
-    ingress {
-      description = "SSH from me"
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["192.226.135.170/32"]
-    }
+  ingress {
+    description = "SSH from me"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["192.226.135.170/32"]
+  }
 
-    ingress {
-      description = "HTTPS from anywhere"
-      from_port = 443
-      to_port = 443
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "HTTPS from anywhere"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-      description = "HTTP from anywhere"
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "HTTP from anywhere"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
+  egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
