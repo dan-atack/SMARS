f0d54b46045bdc97a3fcac87fe6f2dcc34235f94
dan-atack <dan_atack@hotmail.com>
2023-06-16 22:46:34 -0400

    Updated database backup script to created dated save files

--- backupDatabase.sh
+++ backupDatabase.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export $(cat .env | xargs)
-sudo docker exec smars-db-1 sh -c 'exec mongodump -d smars --archive' > ~/backups/smars-backup.archive
-sudo aws s3 cp ~/backups/smars-backup.archive s3://smars-dev-bucket
+# Create one archive file per day, named for the date
+today=$(date +'%Y-%m-%d')
+filename=smars-backup-${date}.archive
+sudo docker exec smars-db-1 sh -c 'exec mongodump -d smars --archive' > ~/backups/${filename}
+sudo aws s3 cp ~/backups/${filename}.archive s3://smars-dev-bucket
