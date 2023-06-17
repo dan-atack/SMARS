5e546e89cb8864840fd2778b2ea73c9e63f52911
dan-atack <dan_atack@hotmail.com>
2023-06-16 22:50:59 -0400

    Fixed error with variable name in database backup script

--- backupDatabase.sh
+++ backupDatabase.sh
@@ -2,6 +2,6 @@
 export $(cat .env | xargs)
 # Create one archive file per day, named for the date
 today=$(date +'%Y-%m-%d')
-filename="smars-backup-${date}.archive"
+filename="smars-backup-${today}.archive"
 sudo docker exec smars-db-1 sh -c 'exec mongodump -d smars --archive' > ~/backups/${filename}
 sudo aws s3 cp ~/backups/${filename} s3://smars-dev-bucket
